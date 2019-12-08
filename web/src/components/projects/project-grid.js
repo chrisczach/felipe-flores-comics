2d1b43b419511d19935f60839a107e6317ee05bf
Chris Czach <ChrisCzach@Chriss-MBP.socal.rr.com>
2019-12-08 15:29:43 -0800

    .

--- web/src/components/projects/project-grid.js
+++ web/src/components/projects/project-grid.js
@@ -8,29 +8,39 @@ import {
   Hidden,
   fade,
   lighten,
+  useMediaQuery,
 } from '@material-ui/core';
 import Masonry from 'react-masonry-css';
 
 import ProjectTile from './project-tile';
 
-const useStyles = makeStyles(theme => ({
-  grid: {
-    display: 'flex',
-    marginLeft: -30,
-    width: 'auto',
-  },
-  column: {
-    paddingLeft: 30,
-    backgroundClip: 'padding-box',
-  },
-}));
+const useStyles = makeStyles(theme => {
+  const gap = theme.spacing(2);
+  return {
+    grid: {
+      display: 'flex',
+      marginLeft: -gap,
+      width: 'auto',
+    },
+    column: {
+      paddingLeft: gap,
+      backgroundClip: 'padding-box',
+    },
+  };
+});
 
 const ProjectGrid = ({ projects, ...props }) => {
+  // @ts-ignore
+  const sm = useMediaQuery(theme => theme.breakpoints.down('sm'));
+  // @ts-ignore
+  const md = useMediaQuery(theme => theme.breakpoints.down('md'));
+
+  const columns = sm ? 1 : md ? 2 : 3;
   const classes = useStyles(props);
   return (
     <Box>
       <Masonry
-        breakpointCols={4}
+        breakpointCols={columns}
         className={classes.grid}
         columnClassName={classes.column}
       >
--- web/src/components/projects/project-tile.js
+++ web/src/components/projects/project-tile.js
@@ -12,6 +12,20 @@ import {
 } from '@material-ui/core';
 import BlockContent from '../block-content';
 
+const useStyles = makeStyles(theme => {
+  const gap = theme.spacing(2);
+  return {
+    root: {
+      cursor: 'pointer',
+      marginBottom: gap,
+      '&:hover': {
+        background: fade(theme.palette.secondary.light, 0.15),
+        boxShadow: theme.shadows[2],
+      },
+    },
+  };
+});
+
 const ProjectTile = ({
   title,
   excerpt,
@@ -25,14 +39,16 @@ const ProjectTile = ({
       },
     },
   },
+  ...props
 }) => {
+  const classes = useStyles(props);
   return (
-    <div>
-      <Img fluid={fluid} />
+    <Paper square className={classes.root} elevation={0}>
+      <Img fluid={fluid} fadeIn durationFadeIn={1500} />
       <Typography variant="h5">{title}</Typography>
       <Typography variant="h6">{slug}</Typography>
       <BlockContent blocks={excerpt} />
-    </div>
+    </Paper>
   );
 };
 
