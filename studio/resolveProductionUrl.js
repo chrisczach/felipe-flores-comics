eece2edbe4b6fb79812b1a944b1e230ebf4b0126
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-12-12 08:34:58 -0800

    .

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -1,7 +1,7 @@
 export default function resolveProductionUrl( {_type, slug: {current: slug}} ) {
   // WIP need to resolve path here
   // return JSON.stringify({_type, slug})
-  const base = 'https://felipe-flores-comics-2846337642.gtsb.io'
-  if(_type === 'page') return `${base}/${slug === '/' ? '' : slug}`;
-  return `${base}/portfolio/${slug}`;
+  const url = 'https://felipe-flores-comics-2846337642.gtsb.io'
+  if(_type === 'page') return `${url}/${slug === '/' ? '' : slug}`;
+  return `${url}/portfolio/${slug}`;
 }
