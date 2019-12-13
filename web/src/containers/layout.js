1f708ea6d95538eeba9ef3ac9a0f5a26d1c396bd
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-12-13 10:36:27 -0800

    .

--- web/src/containers/layout.js
+++ web/src/containers/layout.js
@@ -33,10 +33,10 @@ const query = graphql`
               aspectRatio
             }
           }
-          localFile(width: 24) {
+          localFile(width: 2400) {
             childImageSharp {
               fluid(
-                maxWidth: 24
+                maxWidth: 2400
                 traceSVG: { color: "#8b151b77", background: "#ffd83111" }
               ) {
                 ...GatsbyImageSharpFluid_withWebp_tracedSVG
