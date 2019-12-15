5be769d1b393fe9be77d9d85cc1464716d882e7d
Chris Czach <ChrisCzach@Chriss-MBP.socal.rr.com>
2019-12-14 22:11:24 -0800

    added internal link

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,10 +1,14 @@
 import Figure from './figure/figure';
 import BlocksToMUI from './blocks-to-MUI';
+import BlockLink from './blocks/block-link';
 
 const serializers = {
   types: {
     block: BlocksToMUI,
     figure: Figure,
+  },
+  marks: {
+    link: BlockLink,
   },
 };
 
